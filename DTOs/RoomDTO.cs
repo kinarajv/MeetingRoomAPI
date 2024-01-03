fbd193516269537a94892f285e36d518bb968e12
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2024-01-03 11:57:11 +0700

    Hide RoomId

--- DTOs/RoomDTO.cs
+++ DTOs/RoomDTO.cs
@@ -2,7 +2,7 @@ namespace MeetingRoomAPI.DTOs;
 
 public class RoomDTO
 {
-	public int RoomId { get; set; }
+	// public int RoomId { get; set; }
 	public string Description { get; set; }
 	public int Capacity { get; set; }
 }
